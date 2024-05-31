23a13582b64fc45dfd8527c8660e940860497ad7
Tommy Le <73857087+tommyle27@users.noreply.github.com>
2024-05-30 23:08:38 -0400

    Component Collision
    Unreal Engine tutorial project. Implemented a moveable pawn with a third person movable camera on the x-axis.

---
+++ ComponentCollision/ComponentCollision.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ComponentCollision.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ComponentCollision, "ComponentCollision" );
